ec37a3be3c205437106023dd53f67e9a56e71ab9
mak <mlcnierras@gmail.com>
2024-06-01 16:33:34 +0800

    refactor: toast to one liner function

--- client/composables/usePos.ts
+++ client/composables/usePos.ts
@@ -1,7 +1,7 @@
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 
-function showToast(message: string, type: any) {
+function toasts(message: string, type: any) {
   toast(message, {
     autoClose: 2000,
     theme: "auto",
@@ -10,22 +10,8 @@ function showToast(message: string, type: any) {
   });
 }
 
-export function deleteCartItem() {
-  showToast("Successfully Deleted!", "error");
-}
-
-export function cartClear() {
-  showToast("Cart cleared!", "error");
-}
-
-export function paymentSelect() {
-  showToast("Successfully Paid!", "info");
-}
-
-export function addProductToCart() {
-  showToast("Added Item!", "success");
-}
-
-export function removeProductToCart() {
-  showToast("Item Removed!", "warning");
-}
+export const deleteCartItem = () => { toasts("Successfully Deleted!", "error") }
+export const cartClear = () => { toasts("Cart cleared!", "error") }
+export const paymentSelect = () => { toasts("Successfully Paid!", "info") }
+export const addProductToCart = () => { toasts("Added Item!", "success") }
+export const removeProductToCart = () => { toasts("Item Removed!", "warning") }
